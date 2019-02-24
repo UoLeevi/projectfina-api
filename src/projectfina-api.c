6b2589a5acf2103fff2a1995797482050b9851ba
Leevi Uotinen <uotinen.leevi@hotmail.com>
2019-02-24 18:34:57 +0200

    Add query logic

--- src/projectfina-api.c
+++ src/projectfina-api.c
@@ -9,16 +9,63 @@
 
 #include <stdio.h>
 
+#define HTTP_400_JSON \
+    "{ \"message\": \"bad request\" }"
 #define HTTP_401_JSON \
     "{ \"message\": \"unauthorized\" }"
+#define HTTP_500_JSON \
+    "{ \"message\": \"server error\" }"
 
 static PGconn *pgconn;
+
+static void http_response_with_400(
+    uo_http_msg *http_response)
+{
+    uo_http_msg_set_status_line(http_response, UO_HTTP_400, UO_HTTP_1_1);
+    uo_http_msg_set_content(http_response, HTTP_400_JSON, "application/json", UO_STRLEN(HTTP_400_JSON));
+}
 
 static void http_response_with_401(
     uo_http_msg *http_response)
 {
     uo_http_msg_set_status_line(http_response, UO_HTTP_401, UO_HTTP_1_1);
     uo_http_msg_set_content(http_response, HTTP_401_JSON, "application/json", UO_STRLEN(HTTP_401_JSON));
+}
+
+static void http_response_with_500(
+    uo_http_msg *http_response)
+{
+    uo_http_msg_set_status_line(http_response, UO_HTTP_500, UO_HTTP_1_1);
+    uo_http_msg_set_content(http_response, HTTP_500_JSON, "application/json", UO_STRLEN(HTTP_500_JSON));
+}
+
+static void http_response_get_groups(
+    uo_http_msg *http_response,
+    const char *user_uuid)
+{
+    const char *paramValues_get_groups[1] = { user_uuid };
+
+    PGresult *groups_res = PQexecParams(pgconn,
+        "SELECT '{\"groups\":' || to_json(array_agg(t)) || '}' json "
+          "FROM ("
+            "SELECT * "
+            "FROM groups g JOIN users_x_groups u_x_g ON g.uuid = u_x_g.group_uuid "
+            "WHERE u_x_g.user_uuid = $1::uuid"
+          ") t;",
+        1, NULL, paramValues_get_groups, NULL, NULL, 0);
+
+    if (PQresultStatus(groups_res) != PGRES_TUPLES_OK)
+        http_response_with_500(http_response);
+    else
+    {
+        char *json = PQgetvalue(groups_res, 0, 0);
+        size_t json_len = PQgetlength(groups_res, 0, 0);
+
+        uo_http_msg_set_status_line(http_response, UO_HTTP_200, UO_HTTP_1_1);
+        uo_http_msg_set_content(http_response, json, "application/json", json_len);
+    }
+
+    PQclear(groups_res);
 }
 
 void http_server_before_send_response(
@@ -29,6 +76,7 @@ void http_server_before_send_response(
     uo_http_msg *http_response = http_sess->http_response;
 
     const char *uri = uo_http_request_get_uri(http_request);
+    uo_http_method method = uo_http_request_get_method(http_request);
 
     uo_http_msg_set_header(http_response, "server", "libuo http");
     uo_http_msg_set_header(http_response, "access-control-allow-origin", "*");
@@ -41,18 +89,22 @@ void http_server_before_send_response(
         if (hdr_authorization && sscanf(hdr_authorization, "Bearer %1023s", jwt) == 1)
         {
             char *jwt_payload = uo_jwt_decode_payload(NULL, jwt, strlen(jwt));
+            if (!jwt_payload)
+                goto response_401;
+
             char *jwt_claim_sub = uo_json_find_value(jwt_payload, "sub");
+            if (!jwt_claim_sub)
+                goto response_401;
 
             char user_uuid[37];
+            memcpy(user_uuid, jwt_claim_sub + 1, 36);
+            user_uuid[36] = '\0';
 
-            memcpy(user_uuid, jwt_claim_sub, 36);
+            if (strcmp(uri, "/user/groups") == 0 && method == UO_HTTP_GET)
+                http_response_get_groups(http_response, user_uuid);
+            else
+                http_response_with_400(http_response);
 
-            if (strcmp(uri, "/user/groups") == 0)
-            {
-                if (uo_http_request_get_method(http_request) == UO_HTTP_GET)
-                {
-                }
-            }
         }
         else
 response_401:
